4f3d0794a556ca6f0c63496b3546c6890ad1ac8c
Salomon Baena <dsbaenar@gmail.com>
2025-02-08 22:50:59 -0500

    Transfering data to view

--- src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,7 @@
 package com.salomon.course.springboot.webapp.springboot_web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
     
     @GetMapping("/details") //Petición que es a traves de un URL de un navegador
-    public String details() {
-        return "details";
+    public String details(Model model) {
+        model.addAttribute("title","Hola Mundo Spring Boot");
+        model.addAttribute("name","Salomon");
+        model.addAttribute("lastname","Baena");
+        return "details"; //Retorna el nombre de la vista
     }
 
 }
