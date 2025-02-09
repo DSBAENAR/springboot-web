4986dd250e1d340d97a717f6e9c9ab88c92099f2
Salomon Baena <dsbaenar@gmail.com>
2025-02-08 23:26:37 -0500

    Creation of a new object or class that is User and transfer the data to the view

--- src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,19 +1,26 @@
 package com.salomon.course.springboot.webapp.springboot_web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.salomon.course.springboot.webapp.springboot_web.models.User;
 
 
 @Controller
 public class UserController {
     
     @GetMapping("/details") //Petición que es a traves de un URL de un navegador
-    public String details(Model model) {
-        model.addAttribute("title","Hola Mundo Spring Boot");
-        model.addAttribute("name","Salomon");
-        model.addAttribute("lastname","Baena");
-        return "details"; //Retorna el nombre de la vista
+    public Map<String,Object> details(Model model) {
+        User user = new User("Salomon", "Baena");
+        Map<String,Object> body = new HashMap<>();
+        
+        body.put("title","Hola Mundo Spring Boot");
+        body.put("user",user);
+        return body; //Retorna el nombre de la vista
     }
 
 }
--- src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserRESTController.java
+++ src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserRESTController.java
@@ -4,17 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.salomon.course.springboot.webapp.springboot_web.models.User;
 
 
 @RestController
+@RequestMapping("/api") //Ruta base del api, para accder es h
 public class UserRESTController {
-    @GetMapping("/details2") //Petición que es a traves de un URL de un navegador
+
+    @GetMapping("/details") //Petición que es a traves de un URL de un navegador
     public Map<String,Object> details() {
+        User user = new User("Salomon", "Baena");
         Map<String,Object> body = new HashMap<>();
         body.put("title","Hola Mundo Spring Boot");
-        body.put("name","Salomon");
-        body.put("lastname","Baena");
+        body.put("user",user);
         return body; //Retorna el nombre de la vista
     }
 
