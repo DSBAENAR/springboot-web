bd0a294f4f926541bfe429604ca507ef3e50de40
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-02-13 20:50:56 -0500

    dto in user implemented

--- src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserRESTController.java
+++ src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserRESTController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.salomon.course.springboot.webapp.springboot_web.models.User;
+import com.salomon.course.springboot.webapp.springboot_web.models.dto.UserDto;
 
 
 @RestController
@@ -15,7 +16,20 @@ import com.salomon.course.springboot.webapp.springboot_web.models.User;
 public class UserRESTController {
 
     @GetMapping("/details") //Petición que es a traves de un URL de un navegador
-    public Map<String,Object> details() {
+    public UserDto details() {
+        UserDto userDto = new UserDto();
+        User user = new User("Salomon", "Baena");
+        userDto.setUser(user);
+        userDto.setTitle("Hola Mundo Spring Boot");
+        // Map<String,Object> body = new HashMap<>();
+        // body.put("title","Hola Mundo Spring Boot");
+        // body.put("user",user);
+        return userDto; //Retorna el nombre de la vista
+    }
+
+
+    @GetMapping("/details-map") //Petición que es a traves de un URL de un navegador
+    public Map<String,Object> detailsMap() {
         User user = new User("Salomon", "Baena");
         Map<String,Object> body = new HashMap<>();
         body.put("title","Hola Mundo Spring Boot");
