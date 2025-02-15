2d0ea53131a232c82d03f2a7eae44a6e913adcd7
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-02-14 22:39:51 -0500

    Mostrar listas desde thymeleaf

--- src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.salomon.course.springboot.webapp.springboot_web.controllers;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.salomon.course.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
@@ -22,5 +27,14 @@ public class UserController {
         body.put("user",user);
         return body; //Retorna el nombre de la vista
     }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+        model.addAttribute("users", users);
+        model.addAttribute("title","Listado usuarios:" );
+        return "list";
+    }
+    
 
 }
