e0feeaffb093f7d2fa883247f9058406bc1bbcb0
David Salomon Baena Rubio <dsbaenar@gmail.com>
2025-02-17 23:11:19 -0500

    Using anotation @ModelAtribute

--- src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/salomon/course/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,6 +1,5 @@
 package com.salomon.course.springboot.webapp.springboot_web.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -10,9 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.salomon.course.springboot.webapp.springboot_web.models.User;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -31,13 +30,20 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-        List<User> users = Arrays.asList(new User("Pepe", "Woods"),
+        
+        
+        model.addAttribute("title","Listado usuarios:" );
+        return "list";
+    }
+
+    @ModelAttribute("users") //Para reutilizar datos en cualquier método del controlador, es global
+    public List<User> usersModel(){
+         List<User> users = Arrays.asList(new User("Pepe", "Woods"),
         new User("lalo", "perez","lalo@correo.co"),
         new User("Salomon", "Baena","mail@org.edu"),
         new User("Santiago", "Vega"));
-        model.addAttribute("users", users);
-        model.addAttribute("title","Listado usuarios:" );
-        return "list";
+
+        return users;
     }
     
 
